93e22368563d7a37da28602ab20697229a31ac5e
James McAllister <jrfmcallister@gmail.com>
2018-07-12 20:04:37 +0100

    cleaned up the votes a bit

--- backend/dummie.js
+++ backend/dummie.js
@@ -65,20 +65,6 @@ export default {
         topic: "css"
       }
     },
-    cooking: {
-      "::ffff:192.168.0.116": {
-        vote: "camel",
-        topic: "cooking"
-      },
-      "::ffff:192.168.0.120": {
-        vote: "mash",
-        topic: "cooking"
-      },
-      "::ffff:192.168.0.185": {
-        vote: "mash",
-        topic: "cooking"
-      }
-    },
     coffee: {
       "::ffff:192.168.0.192": {
         vote: "mash",
@@ -125,146 +111,6 @@ export default {
         topic: "memes"
       }
     },
-    "james sucks": {
-      "::ffff:192.168.0.136": {
-        vote: "camel",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.169": {
-        vote: "camel",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.151": {
-        vote: "camel",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.190": {
-        vote: "camel",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.192": {
-        vote: "camel",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.180": {
-        vote: "duck",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.116": {
-        vote: "mash",
-        topic: "james sucks"
-      },
-      "::ffff:192.168.0.137": {
-        vote: "potato",
-        topic: "james sucks"
-      }
-    },
-    sing: {
-      "::ffff:192.168.0.135": {
-        vote: "donut",
-        topic: "sing"
-      }
-    },
-    spacecakes: {
-      "::ffff:192.168.0.135": {
-        vote: "mash",
-        topic: "spacecakes"
-      },
-      "::ffff:192.168.0.151": {
-        vote: "camel",
-        topic: "spacecakes"
-      },
-      "::ffff:192.168.0.192": {
-        vote: "mash",
-        topic: "spacecakes"
-      },
-      "::ffff:192.168.0.190": {
-        vote: "camel",
-        topic: "spacecakes"
-      },
-      "::ffff:192.168.0.136": {
-        vote: "potato",
-        topic: "spacecakes"
-      },
-      "::ffff:192.168.0.137": {
-        vote: "mash",
-        topic: "spacecakes"
-      }
-    },
-    "james sucks again...": {
-      "::ffff:192.168.0.136": {
-        vote: "mash",
-        topic: "james sucks again..."
-      },
-      "::ffff:192.168.0.116": {
-        vote: "duck",
-        topic: "james sucks again..."
-      },
-      "::ffff:192.168.0.152": {
-        vote: "camel",
-        topic: "james sucks again..."
-      }
-    },
-    singing: {
-      "::ffff:192.168.0.135": {
-        vote: "donut",
-        topic: "singing"
-      }
-    },
-    camels: {
-      "::ffff:192.168.0.192": {
-        vote: "camel",
-        topic: "camels"
-      },
-      "::ffff:192.168.0.135": {
-        vote: "duck",
-        topic: "camels"
-      }
-    },
-    "again camels": {
-      "::ffff:192.168.0.135": {
-        vote: "donut",
-        topic: "again camels"
-      },
-      "::ffff:192.168.0.136": {
-        vote: "donut",
-        topic: "again camels"
-      },
-      "::ffff:192.168.0.137": {
-        vote: "potato",
-        topic: "again camels"
-      }
-    },
-    pub: {
-      "::ffff:192.168.0.135": {
-        vote: "potato",
-        topic: "pub"
-      }
-    },
-    "rubber duck": {
-      "::ffff:192.168.0.135": {
-        vote: "donut",
-        topic: "rubber duck"
-      },
-      "::ffff:192.168.0.136": {
-        vote: "donut",
-        topic: "rubber duck"
-      }
-    },
-    "react is for n00bs": {
-      "::ffff:192.168.0.135": {
-        vote: "potato",
-        topic: "react is for n00bs"
-      },
-      "::ffff:192.168.0.169": {
-        vote: "mash",
-        topic: "react is for n00bs"
-      },
-      "::ffff:192.168.0.137": {
-        vote: "duck",
-        topic: "react is for n00bs"
-      }
-    },
     "lunch club": {
       "::ffff:192.168.0.137": {
         vote: "camel",
@@ -275,21 +121,9 @@ export default {
   topics: {
     react: "",
     css: "",
-    cooking: "",
     breakdancing: "",
     coffee: "",
     memes: "",
-    sing: "",
-    singing: "",
-    "james sucks": "",
-    "again camels": "",
-    pub: "",
-    spacecakes: "",
-    camels: "",
-    "james sucks again...": "",
-    "react is for n00bs": "",
-    "rubber duck": "",
-    rubberducks: "",
     "lunch club": ""
   }
 };
